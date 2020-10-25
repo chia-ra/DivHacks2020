427ac1dcf09a373e9afa237baa4b2eb531003eca
Chiara Seim <chiaraseim@gmail.com>
2020-10-24 20:58:03 -0700

    Adds a back button to top of every applicable screen, as well as buttons on recycle screen for each resource. Also creates a button divider drawable to use as spacing between buttons

--- app/src/main/java/com/example/thinkgreen/RecycleActivity.java
+++ app/src/main/java/com/example/thinkgreen/RecycleActivity.java
@@ -2,7 +2,9 @@ package com.example.thinkgreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RecycleActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class RecycleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycle);
+    }
+    public void metalMessage(View view) {
+        // Do something in response to button click
+        Intent intent = new Intent (this, RecycleActivity.class);
+        startActivity(intent);
+    }
+    public void paperMessage(View view) {
+        // Do something in response to button click
+        Intent intent = new Intent (this, RecycleActivity.class);
+        startActivity(intent);
+    }
+    public void plasticMessage(View view) {
+        // Do something in response to button click
+        Intent intent = new Intent (this, RecycleActivity.class);
+        startActivity(intent);
+    }
+    public void glassMessage(View view) {
+        // Do something in response to button click
+        Intent intent = new Intent (this, RecycleActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/thinkgreen/ReuseActivity.java
+++ app/src/main/java/com/example/thinkgreen/ReuseActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class ReuseActivity extends AppCompatActivity {
@@ -13,4 +14,5 @@ public class ReuseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reuse);
     }
+
 }
