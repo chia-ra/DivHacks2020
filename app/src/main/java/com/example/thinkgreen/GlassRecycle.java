617f37a64011a0e97963903a1fd1bec280c2b069
Chiara Seim <chiaraseim@gmail.com>
2020-10-25 02:51:43 -0700

    adds a calculator function to applicable recycle pages

--- app/src/main/java/com/example/thinkgreen/GlassRecycle.java
+++ app/src/main/java/com/example/thinkgreen/GlassRecycle.java
@@ -2,8 +2,11 @@ package com.example.thinkgreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class GlassRecycle extends AppCompatActivity {
@@ -29,5 +32,23 @@ public class GlassRecycle extends AppCompatActivity {
                 "\n Pyrex Dishes";
         textView.setText(para);
         textView.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    public void calculator(View view) {
+        // Do something in response to button click
+        EditText et1=findViewById(R.id.editTextNumber);
+        EditText et2=findViewById(R.id.editTextNumber2);
+        String temp=et1.getText().toString();
+        String temp2=et2.getText().toString();
+        int value=0;
+        if (!"".equals(temp)){
+            value+=Integer.parseInt(temp) * 5;
+        }
+        if (!"".equals(temp2)){
+            value+=Integer.parseInt(temp2) * 10;
+        }
+        TextView t = (TextView)findViewById(R.id.textView6);
+        t.setText("Exchange for " + value + "¢");
+
     }
 }
--- app/src/main/java/com/example/thinkgreen/PlasticRecycle.java
+++ app/src/main/java/com/example/thinkgreen/PlasticRecycle.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class PlasticRecycle extends AppCompatActivity {
@@ -28,5 +30,22 @@ public class PlasticRecycle extends AppCompatActivity {
                 "\n Vinyl";
         textView.setText(para);
         textView.setMovementMethod(new ScrollingMovementMethod());
+    }
+    public void calculator(View view) {
+        // Do something in response to button click
+        EditText et1=findViewById(R.id.editTextNumber);
+        EditText et2=findViewById(R.id.editTextNumber2);
+        String temp=et1.getText().toString();
+        String temp2=et2.getText().toString();
+        int value=0;
+        if (!"".equals(temp)){
+            value+=Integer.parseInt(temp) * 5;
+        }
+        if (!"".equals(temp2)){
+            value+=Integer.parseInt(temp2) * 10;
+        }
+        TextView t = (TextView)findViewById(R.id.textView6);
+        t.setText("Exchange for " + value + "¢");
+
     }
 }
